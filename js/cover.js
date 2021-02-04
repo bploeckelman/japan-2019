4c921bb4dd6591cf09d5bd02a9598c8a321186f0
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2021-02-03 22:33:12 -0600

    Activate and pulse svg circles when hovering circle or associated heading

--- js/cover.js
+++ js/cover.js
@@ -1,13 +1,47 @@
 var map = document.getElementById("map");
-var areas = map.getElementsByTagName("a");
 
+var areas = map.getElementsByTagName("a");
 for (var i = 0; i < areas.length; i++) {
   areas[i].onmouseover = function() {
-    var t = document.getElementById(this.getAttribute("xlink:href").substr(1));
-    t.classList.toggle("active");
+    var circle = this.getElementsByTagName("circle")[0];
+    var animate = circle.getElementsByTagName("animate")[0];
+    animate.setAttribute("dur", "0.66s");
+
+    var tag = document.getElementById(this.getAttribute("xlink:href").substr(1));
+    tag.classList.add("active");
   }
   areas[i].onmouseout = function() {
-    var t = document.getElementById(this.getAttribute("xlink:href").substr(1));
-    t.classList.toggle("active");
+    var circle = this.getElementsByTagName("circle")[0];
+    var animate = circle.getElementsByTagName("animate")[0];
+    animate.setAttribute("dur", "3s");
+
+    var tag = document.getElementById(this.getAttribute("xlink:href").substr(1));
+    tag.classList.remove("active");
+  }
+}
+
+var tags = document.getElementsByClassName("tag");
+for (var i = 0; i < tags.length; i++) {
+  tags[i].onmouseover = function() {
+    this.classList.add("active");
+    for (var j = 0; j < areas.length; j++) {
+      if (areas[j].getAttribute("xlink:href").substr(1) === this.getAttribute("id")) {
+        var circle = areas[j].getElementsByTagName("circle")[0];
+        circle.classList.add("active");
+        var animate = circle.getElementsByTagName("animate")[0];
+        animate.setAttribute("dur", "0.66s");
+      }
+    }
+  }
+  tags[i].onmouseout = function() {
+    this.classList.remove("active");
+    for (var j = 0; j < areas.length; j++) {
+      if (areas[j].getAttribute("xlink:href").substr(1) === this.getAttribute("id")) {
+        var circle = areas[j].getElementsByTagName("circle")[0];
+        circle.classList.remove("active");
+        var animate = circle.getElementsByTagName("animate")[0];
+        animate.setAttribute("dur", "3s");
+      }
+    }
   }
 }
